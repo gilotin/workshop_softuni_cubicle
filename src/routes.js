eb1485bba345ef13cb8839cfcd76001bad9337c6
Nikolay <103648794+gilotin@users.noreply.github.com>
2023-01-26 11:59:15 +0200

    added additional controllers

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,12 @@
 const router = require("express").Router();
 const cubeController = require('./controllers/cubeController');
+const homeController = require("./controllers/homeController");
 
 
 
-router.get("/", (req, res) => {
-    res.render("index");
-});
+router.get("/", homeController.getAboutPage);
 
-router.get("/about", (req, res) => {
-    res.render("about");
-});
+router.get("/about", homeController.getHomePage);
 
 router.get("/create", cubeController.getCreateCube);
 
