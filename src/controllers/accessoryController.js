7cbc6316493b206510828b306cbf8d7a2fee3e02
Nikolay <103648794+gilotin@users.noreply.github.com>
2023-01-31 00:13:01 +0200

    Added accessory/create in DB

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,7 +1,19 @@
 const router = require("express").Router();
+const Accessory = require("../models/Accessory");
 
 router.get("/create", (req, res) => {
-    res.render('accessory/create');
+    res.render("accessory/create");
+});
+
+router.post("/create", async (req, res) => {
+    console.log(req.body);
+    const { name, description, imageUrl } = req.body;
+
+    // await Accessory.create({ name, description, imageUrl })   <-- both are the same;
+    const accessory = new Accessory({ name, description, imageUrl });
+    await accessory.save();
+
+    res.redirect("/");
 });
 
 module.exports = router;
